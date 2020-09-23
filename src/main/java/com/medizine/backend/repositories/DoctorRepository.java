e405163a382f06c49b6e31b33f01417cfb46a6b7
sankrityaakash <sankrityaakash@gmail.com>
2020-09-23 20:22:25 +0530

    Added API Error handling methods

--- src/main/java/com/medizine/backend/repositories/DoctorRepository.java
+++ src/main/java/com/medizine/backend/repositories/DoctorRepository.java
@@ -1,10 +1,10 @@
 package com.medizine.backend.repositories;
 
-import com.medizine.backend.models.DoctorEntity;
+import com.medizine.backend.dto.Doctor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DoctorRepository extends MongoRepository<DoctorEntity, String> {
+public interface DoctorRepository extends MongoRepository<Doctor, String> {
 
 }
