849b4ee9e9c42d13728e53848ba698a41485e368
AKASH SANKRITYA <30947507+aakashsankritya@users.noreply.github.com>
2020-10-10 10:37:33 +0530

    -- Added support for Appointments
    Signed-off-by: sankrityaakash <sankrityaakash@gmail.com>

--- src/test/java/com/medizine/backend/MedizineBackendApplicationTests.java
+++ src/test/java/com/medizine/backend/MedizineBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MedizineBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
