69c836483530e3aa020974948d2bd6530e079fd4
sankrityaakash <sankrityaakash@gmail.com>
2020-11-03 17:36:37 +0530

    Add Zoom Meeting Feature
    Signed-off-by: sankrityaakash <sankrityaakash@gmail.com>

--- src/main/java/com/medizine/backend/repositories/ZoomRepository.java
+++ src/main/java/com/medizine/backend/repositories/ZoomRepository.java
@@ -1,12 +1,12 @@
 package com.medizine.backend.repositories;
 
-import com.medizine.backend.dto.ModuleType;
 import com.medizine.backend.dto.ZoomMeeting;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ZoomRepository extends MongoRepository<ZoomMeeting, String> {
 
-    ZoomMeeting findByModuleIdAndModuleType(String moduleId, ModuleType moduleType);
+    ZoomMeeting findByHostId(ObjectId hostId);
 }
